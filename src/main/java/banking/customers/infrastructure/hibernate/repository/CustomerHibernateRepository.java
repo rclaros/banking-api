3d949c899376fe637cab767557366ba1339ea3de
reyiclaros@gmail.com <reyiclaros@gmail.com>
2019-07-10 00:45:04 -0500

    Native Query

--- src/main/java/banking/customers/infrastructure/hibernate/repository/CustomerHibernateRepository.java
+++ src/main/java/banking/customers/infrastructure/hibernate/repository/CustomerHibernateRepository.java
@@ -10,28 +10,54 @@ import org.springframework.stereotype.Repository;
 import banking.common.infrastructure.hibernate.repository.BaseHibernateRepository;
 import banking.customers.domain.entity.Customer;
 import banking.customers.domain.repository.CustomerRepository;
+import org.hibernate.Query;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.BooleanType;
+import org.hibernate.type.LongType;
+import org.hibernate.type.StringType;
 
 @Transactional
 @Repository
 public class CustomerHibernateRepository extends BaseHibernateRepository<Customer> implements CustomerRepository {
-	public Customer get(long customerId) {
-		Customer customer = null;
-		customer = getSession().get(Customer.class, customerId);
-		return customer;
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Customer> get(int page, int pageSize) {
-		List<Customer> customers = null;
-		Criteria criteria = getSession().createCriteria(Customer.class);
-		criteria.setFirstResult(page);
-		criteria.setMaxResults(pageSize);
-		customers = criteria.list();
-		return customers;
-	}
-	
-	public Customer save(Customer customer) {
-		return super.save(customer);
-	}
+
+    public Customer get(long customerId) {
+        Customer customer = null;
+        customer = getSession().get(Customer.class, customerId);
+        return customer;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Customer> get(int page, int pageSize) {
+        Query query = getSession().createSQLQuery(""
+                + "  SELECT "
+                + "  cu.customer_id as id,"
+                + "  cu.first_name originAccountNumber,"
+                + "  cu.last_name destinationAccountNumber,"
+                + "  cu.active isActive "
+                + "FROM "
+                + "	customer cu "
+                + "INNER JOIN ( "
+                + "	SELECT "
+                + "	   customer_id as id "
+                + "	FROM "
+                + "	   customer "
+                + "	ORDER BY "
+                + "	   id ASC "
+                + "	LIMIT " + page + "," + pageSize + " "
+                + "     ) AS t "
+                + "ON t.id = cu.customer_id "
+                + "ORDER BY cu.customer_id ASC")
+                .addScalar("id", new LongType())
+                .addScalar("firstName", new StringType())
+                .addScalar("lastName", new StringType())
+                .addScalar("isActive", new BooleanType())
+                .setResultTransformer(Transformers.aliasToBean(Customer.class));
+        List<Customer> customers = query.list();
+        return customers;
+    }
+
+    public Customer save(Customer customer) {
+        return super.save(customer);
+    }
 }
--- src/main/java/banking/transactions/infrastucture/hibernate/repository/HistoryTransactionHibernateRepository.java
+++ src/main/java/banking/transactions/infrastucture/hibernate/repository/HistoryTransactionHibernateRepository.java
@@ -6,6 +6,7 @@
 package banking.transactions.infrastucture.hibernate.repository;
 
 import banking.common.infrastructure.hibernate.repository.BaseHibernateRepository;
+import banking.customers.domain.entity.Customer;
 import banking.transactions.application.dto.TransactionDto;
 import banking.transactions.domain.repository.HistoryTransactionRepository;
 import banking.transactions.entity.HistoryTransaction;
@@ -13,8 +14,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.BigDecimalType;
+import org.hibernate.type.BooleanType;
+import org.hibernate.type.DateType;
+import org.hibernate.type.IntegerType;
+import org.hibernate.type.LongType;
+import org.hibernate.type.StringType;
+import org.hibernate.type.TimestampType;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,27 +38,33 @@ public class HistoryTransactionHibernateRepository extends BaseHibernateReposito
 
     @Override
     public List<TransactionDto> getTransactions(Date start_transaction, Date end_trasaction, int page, int pageSize) throws Exception {
-        List<TransactionDto> transactions = new ArrayList<>();
-        Criteria criteria = getSession().createCriteria(HistoryTransaction.class, "c");
-        if (start_transaction != null) {
-            criteria.add(Restrictions.gt("c.created", start_transaction));
-        }
-        if (end_trasaction != null) {
-            criteria.add(Restrictions.gt("c.created", end_trasaction));
-        }
-        criteria.addOrder(Order.asc("c.created"));
-        criteria.setFirstResult(page);
-        criteria.setMaxResults(pageSize);
-        List<HistoryTransaction> history_transactions = criteria.list();
-        for (HistoryTransaction transactionDAO : history_transactions) {
-            TransactionDto transaction = new TransactionDto();
-            transaction.setId(transactionDAO.getId());
-            transaction.setOriginAccountNumber(transactionDAO.getOrigin().getNumber());
-            transaction.setDestinationAccountNumber(transactionDAO.getDestination().getNumber());
-            transaction.setAmount(transactionDAO.getAmount());
-            transaction.setCreate_time(transactionDAO.getCreated());
-            transactions.add(transaction);
-        }
+        Query query = getSession().createSQLQuery(""
+                + "  SELECT "
+                + "	cu.bank_history_id AS id, "
+                + "	origin.number originAccountNumber, "
+                + "	destination.number destinationAccountNumber, "
+                + "	cu.amount amount, "
+                + "	cu.created create_time "
+                + "FROM "
+                + "	bank_transaction cu "
+                + "INNER JOIN ( "
+                + "	SELECT "
+                + "		bank_history_id id "
+                + "	FROM "
+                + "		bank_transaction "
+                + "	ORDER BY "
+                + "		created desc "
+                + "	LIMIT " + page + "," + pageSize + " "
+                + ") AS t ON t.id = cu.bank_history_id "
+                + "INNER JOIN bank_account origin on origin.bank_account_id=cu.account_origin_id "
+                + "INNER JOIN bank_account destination on destination.bank_account_id=cu.account_destination_id")
+                .addScalar("id", new IntegerType())
+                .addScalar("originAccountNumber", new StringType())
+                .addScalar("destinationAccountNumber", new StringType())
+                .addScalar("amount", new BigDecimalType())
+                .addScalar("create_time", new TimestampType())
+                .setResultTransformer(Transformers.aliasToBean(TransactionDto.class));
+        List<TransactionDto> transactions = query.list();
         return transactions;
     }
 
@@ -58,7 +74,6 @@ public class HistoryTransactionHibernateRepository extends BaseHibernateReposito
         Criteria criteria = getSession().createCriteria(HistoryTransaction.class, "c");
         criteria.createAlias("c.origin", "or");
         criteria.add(Restrictions.eq("or.id", acountId));
-
         if (start_transaction != null) {
             criteria.add(Restrictions.gt("c.created", start_transaction));
         }
